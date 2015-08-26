8608ec95ea127d8121ffec8e78872a9b59010daa
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-08-26 10:56:45 +0200

    add public directory

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -16,7 +16,7 @@ class OgdchPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'switzerland')
+        toolkit.add_public_directory(config_, 'public')
     
     # IValidators
 
