29b26270043c0088f2851d14c3c73744a284aacd
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-09-02 16:26:16 +0200

    Add specific fields to search

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -1,6 +1,10 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import pylons
+import json
+import pprint
+import logging
+log = logging.getLogger(__name__)
 
 from ckanext.switzerland import validators
 
@@ -33,18 +37,18 @@ class OgdchPackagePlugin(plugins.SingletonPlugin):
 
     # IPackageController
 
-    def before_view(self, data_dict):
+    def before_view(self, pkg_dict):
         desired_lang_code = pylons.request.environ['CKAN_LANG']
-        for key, value in data_dict.iteritems():
+        for key, value in pkg_dict.iteritems():
             if isinstance(value, dict):
-                data_dict[key] = self.get_language_value(value, desired_lang_code, default_value=value)
-        for resource in data_dict['resources']:
+                pkg_dict[key] = self.get_language_value(value, desired_lang_code, default_value=value)
+        for resource in pkg_dict['resources']:
             if not resource['name'] and resource['title']:
                 resource['name'] = resource['title']
             for key, value in resource.iteritems():
                 if isinstance(value, dict):
                     resource[key] = self.get_language_value(value, desired_lang_code, default_value=value)
-        return data_dict
+        return pkg_dict
 
     def get_language_value(self, lang_dict, desired_lang_code, default_value=''):
         try:
@@ -63,3 +67,36 @@ class OgdchPackagePlugin(plugins.SingletonPlugin):
             except (KeyError, IndexError, ValueError):
                 continue
         return default_value
+
+    def before_index(self, pkg_dict):
+        extract_title = LangToString('title')
+        validated_dict = json.loads(pkg_dict['validated_data_dict'])
+        
+        log.debug(pprint.pformat(validated_dict['title']))
+
+        pkg_dict['res_name'] = map(extract_title, validated_dict[u'resources'])
+        pkg_dict['title_string'] = extract_title(validated_dict)
+        pkg_dict['notes'] = LangToString('notes')(validated_dict)
+
+        pkg_dict['title_de'] = validated_dict['title']['de']
+        pkg_dict['title_fr'] = validated_dict['title']['fr']
+        pkg_dict['title_it'] = validated_dict['title']['it']
+        pkg_dict['title_en'] = validated_dict['title']['en']
+        log.debug(pprint.pformat(pkg_dict))
+        return pkg_dict
+   
+    def before_search(self, search_params):
+        log.debug(pprint.pformat(search_params))
+        return search_params
+
+
+class LangToString(object):
+    def __init__(self, attribute):
+        self.attribute = attribute
+
+    def __call__(self, data_dict):
+        lang = data_dict[self.attribute]
+        return (
+            '%s - %s - %s - %s'
+            % (lang['de'], lang['fr'], lang['it'], lang['en'])
+        )
