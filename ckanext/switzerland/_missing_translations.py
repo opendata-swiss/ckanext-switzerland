7dfb1ab56dea6708ff6e42420ceaafab9d3a1455
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-03 17:22:16 +0100

    add missing translation files to add ckan translations

---
+++ ckanext/switzerland/_missing_translations.py
@@ -0,0 +1,9 @@
+'''
+This file contains missing translation strings which can't be extracted from code.
+Eg. A string which is dynamically passed to a translation function _( var ) or CKAN translation we want to overwrite.
+'''
+
+exit()
+
+// CKAN
+_('About')
