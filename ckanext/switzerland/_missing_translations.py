03687ecb5f47c3cfd1dda29887a0613c4f52f240
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-03 22:48:42 +0100

    remove unused missing translations file

--- ckanext/switzerland/_missing_translations.py
+++
@@ -1,9 +0,0 @@
-'''
-This file contains missing translation strings which can't be extracted from code.
-Eg. A string which is dynamically passed to a translation function _( var ) or CKAN translation we want to overwrite.
-'''
-
-exit()
-
-// CKAN
-_('About')
