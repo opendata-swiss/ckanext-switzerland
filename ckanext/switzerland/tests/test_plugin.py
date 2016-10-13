d3b8078d04a8ed2a5d5fe8176487eab69037a586
Juerg Hunziker <juerg.hunziker@liip.ch>
2016-10-13 17:19:36 +0200

    implement test for prepare_resources_format function

--- ckanext/switzerland/tests/test_plugin.py
+++ ckanext/switzerland/tests/test_plugin.py
@@ -1,5 +1,33 @@
 """Tests for plugin.py."""
 import ckanext.switzerland.plugin as plugin
+from nose.tools import *  # noqa
+import mock
+import sys
 
-def test_plugin():
-    pass
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+class TestPlugin(unittest.TestCase):
+    def setUp(self):
+        self.pkg_dict = {
+            'resources': [
+                {
+                    'format': 'XML'
+                },
+                {
+                    'format': 'unknown_format'
+                }
+            ]
+        }
+
+    def tearDown(self):
+        self.pkg_dict = None
+
+    def test_prepare_resources_format(self):
+        ogdch_language_plugin = plugin.OgdchLanguagePlugin()
+
+        pkg_dict_cleaned_formats = ogdch_language_plugin._prepare_resources_format(self.pkg_dict.copy())
+        self.assertEquals(pkg_dict_cleaned_formats['resources'][0]['format'], self.pkg_dict['resources'][0]['format'])
+        self.assertIsNone(pkg_dict_cleaned_formats['resources'][1]['format'])
