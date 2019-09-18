8e29012c4998949ab5563d3047683d2acefd2570
Sabine Maennel <sabine.maennel@gmail.com>
2019-09-18 10:14:14 +0200

    correct the find statement

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -43,7 +43,7 @@ echo "Setting up Solr..."
 #sudo cp solr_schema.xml /etc/solr/conf/schema.xml
 #sudo service jetty restart
 printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo find . -name 'schema/xml'
+sudo find . -name 'schema.xml'
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
