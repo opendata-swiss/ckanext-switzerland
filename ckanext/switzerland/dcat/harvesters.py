9894832c6fbd9bb8597345392ca53b95728d9d4c
Rae Knowler <rae.knowler@liip.ch>
2019-10-07 15:04:43 +0200

    Fix indentation error.

--- ckanext/switzerland/dcat/harvesters.py
+++ ckanext/switzerland/dcat/harvesters.py
@@ -35,10 +35,10 @@ class SwissDCATRDFHarvester(DCATRDFHarvester):
             if not isinstance(excluded_dataset_identifiers, list):
                 raise ValueError('excluded_dataset_identifiers must be '
                                  'a list of strings')
-                if not all(isinstance(item, basestring)
-                           for item in excluded_dataset_identifiers):
-                    raise ValueError('excluded_dataset_identifiers must be '
-                                     'a list of strings')
+            if not all(isinstance(item, basestring)
+                       for item in excluded_dataset_identifiers):
+                raise ValueError('excluded_dataset_identifiers must be '
+                                 'a list of strings')
 
         return source_config
 
