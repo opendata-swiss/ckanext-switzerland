3a28ec0adc069d7b224f170a00ff366739df0742
Sabine Maennel <sabine.maennel@gmail.com>
2019-11-09 23:04:56 +0100

    add more tests

--- ckanext/switzerland/tests/test_dcat_helpers.py
+++ ckanext/switzerland/tests/test_dcat_helpers.py
@@ -17,3 +17,13 @@ class TestHelpers(unittest.TestCase):
         page_count = 2
         result = tk_dcat._get_shacl_page_identifier(page_count)
         self.assertEquals(result, 'page-2')
+
+    def test_get_shacl_shape_identifier(self):
+        shacl_file = 'ech-0200.shacl.ttl'
+        result = tk_dcat._get_shacl_shape_identifier(shacl_file)
+        self.assertEquals(result, 'ech-0200')
+
+    def test_get_shacl_command_from_config(self):
+        result = tk_dcat.get_shacl_command_from_config()
+        self.assertEquals(result.split('/')[-1], 'shacl')
+
