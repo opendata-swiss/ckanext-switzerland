e5ab62b02748df55b3c3ed15930efaf8baa472b7
Sabine Maennel <sabine.maennel@gmail.com>
2019-11-10 17:15:38 +0100

    wip

--- ckanext/switzerland/tests/test_swiss_shacl_profile_parse.py
+++ ckanext/switzerland/tests/test_swiss_shacl_profile_parse.py
@@ -4,6 +4,7 @@ import os
 
 import unittest
 import rdflib
+from rdflib.namespace import NamespaceManager
 
 from ckanext.switzerland.dcat.profiles import SHACL
 
@@ -13,13 +14,11 @@ class TestSwissShaclProfileParsing(unittest.TestCase):
     def setUp(self):
         resultfile = os.path.join(os.path.dirname(__file__),
                             'fixtures',
-                            'shacl-data.ttl')
+                            'shaclresult.ttl')
         self.r = rdflib.Graph()
         self.r.parse(resultfile, format='turtle')
         self.r.bind('sh', SHACL)
-        print("hello from setup")
-        print(len(self.r))
-        self.r.namespace_manager = rdflib.NamespaceManager(self.r)
+        self.r.namespace_manager = NamespaceManager(self.r)
 
     def test_len_shacl_result(self):
         self.assertEqual(len(self.r), 58)
