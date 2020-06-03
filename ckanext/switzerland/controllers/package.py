abf8f3f68445af5659d0f735bdb57827691152e1
Sabine Maennel <sabine.maennel@gmail.com>
2020-06-03 13:48:38 +0200

    move controllers into a directory of their own and change routes to use the controllers

---
+++ ckanext/switzerland/controllers/package.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+import logging
+import os
+import ckan.controllers.package as package
+import ckanext.switzerland.backend_helpers as bh
+
+log = logging.getLogger(__name__)
+
+
+class OgdchPackageController(package.PackageController):
+
+    def _search_template(self, package_type):
+        return bh.ogdch_template_choice(
+            template_frontend=os.path.join('package', 'search_ogdch.html'),
+            template_backend=os.path.join('package', 'search.html')
+        )
